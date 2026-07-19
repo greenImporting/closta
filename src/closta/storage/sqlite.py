cc315cef2088f43bd1aaceb4fb6f319124186a49
greenImporting <134869548+greenImporting@users.noreply.github.com>
2026-07-19 11:11:56 +0900

    another intermediate. commit before i break everything. added delete tasks

--- src/closta/storage/sqlite.py
+++ src/closta/storage/sqlite.py
@@ -1,1 +1,54 @@
 # use sqlite3 to save task name, description, importance
+import sqlite3
+
+db_name = 'closta.db'
+
+def init_db():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute(
+        ''' CREATE TABLE IF NOT EXISTS tasks
+            (id INTEGER PRIMARY KEY AUTOINCREMEMNT,
+            name TEXT NOT NULL
+            description TEXT,
+            importance INTEGER)'''
+        )
+
+    conn.commit()
+    conn.close()
+
+def save_task(name, description=None, importance=0):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('INSERT INTO tasks (name, description, importance) values (?, ?, ?)',
+              (name, description, importance)
+    )
+
+    conn.commit()
+    conn.close()
+    
+def load_tasks():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()     
+
+    c.execute('SELECT id, name, description, importance FROM tasks')
+    rows = c.fetchall()
+
+    conn.close()
+
+    for row in rows:
+        build_task(row[0], row[1], row[2], row[3])
+
+def delete_callback(sender, app_data, usr_data):
+    task_id = usr_data
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+
+    conn.commit()
+    conn.close()
+
+    dpg.delete_item(dpg.get_parent(sender))
--- src/closta/window/main.py
+++ src/closta/window/main.py
@@ -1,60 +1,113 @@
 import dearpygui.dearpygui as dpg
 import pywinctl as pwc
+import sqlite3
+from closta.storage.sqlite import delete_callback, save_task, init_db, db_name
+from pathlib import Path
 
 WINDOW_RUNNING = False
-_i_am_closed = False
+_graceful_tray_exit = False
 
-def build_table():
-    with dpg.table(header_row=False):
-        dpg.add_table_column()
-        #TODO: if logic here if first run
-        with dpg.table_row():
-            with dpg.table_cell():
-                dpg.add_text("example data :P")
+"""
+[x] build tasks
+[x] add tasks
+[x] delete tasks
+[ ] edit tasks
+[ ] import tasks from db
 
-def build_task():
+current issues im noticing.
+- not importing correctly
+- saving is a bitch
+
+"""
+def newbie_checker():
+    # checks if youre new. if so, init db. TODO: give lovely welcome message.
+    db_path = Path(__file__).resolve().parent / ".." / ".." / ".."
+    uhh = db_path / "closta.db"
+    if not uhh.is_file():
+        print("welcome! initialising a db.")
+        init_db()
+
+def build_task(task_id, heading, description, importance: int, parent="task_container"):
     """
     function to be ran to create a task. arguments to be
     title, description, importance TODO:extra metadata such as time
 
     will just add those into main window as like a list, with a button of completed and delete.
     """
-    #example data
-    _example_head = "Lorem ipsum"
-    _example_desc = "dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    _example_importance = 1 # 0: !, 1: !!, 2: !!!
+    with dpg.child_window(height=200, horizontal_scrollbar=False, parent=parent):
+        dpg.add_text(heading, wrap=0 )
+        dpg.add_text(description,wrap=0)
+        dpg.add_button(label="delete", user_data=task_id, callback=delete_callback)
+        # dpg.add_button(label="edit", user_data=task_id, callback=edit_callback)
+
+def load_tasks_ui(parent="task_container"):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('SELECT id, name, description, importance FROM tasks')
+    rows = c.fetchall()
+    conn.close()
+    for row in rows:
+        build_task(row[0], row[1], row[2], row[3], parent=parent)
 
 
-def delete_task():
-    pass
+def new_task(sender, app_data):
+
+    def refresh_task_list():
+        dpg.delete_item("task_container", children_only=True)
+        load_tasks_ui()
+
+    def save_new_task_callback(sender, app_data):
+        heading = dpg.get_value("heading_input")
+        description = dpg.get_value("desc_input")
+        importance = dpg.get_value("imp_dropdown")
+        save_task(heading, description, importance)
+        dpg.delete_item("new_task_win")
+        refresh_task_list()
+
+    with dpg.window(tag="new_task_win", label="new task"):
+        dpg.add_input_text(tag="heading_input", label="heading")
+        dpg.add_input_text(tag="desc_input", label="description")
+        dpg.add_combo(items=[0,1,2], tag="imp_dropdown", default_value=0)
+        dpg.add_button(label="add task", callback=save_new_task_callback)
 
 
 def create_window():
+    newbie_checker()
     dpg.create_context()
     dpg.create_viewport(title="closta", width=300, height=600, decorated=False)
     with dpg.window(tag="closta"):
-        dpg.add_text("Lorem ipsum")
-        build_table()
+        dpg.add_text("closta", tag="h")
+        dpg.add_button(label="add task", callback=new_task)
+    
+        with dpg.group(tag="task_container"):
+            pass
+            # this will allow us to isolate task display into a single container
+            # itll make it dead easy to clear existing task widgets when we add a new task.
+    load_tasks_ui()
+
 
 def spawn_window():
-    global WINDOW_RUNNING, _i_am_closed
-    _i_am_closed = False
-    if WINDOW_RUNNING == False:
-        # add check logic here for if there is win open already 
-        # so multiple windows dont open (clicking on tray)
-        # checking using is dearpygui running before creating everythign will give it a heart attack
+    global WINDOW_RUNNING, _graceful_tray_exit
+    _graceful_tray_exit = False
+    if WINDOW_RUNNING:
+        print("window is runnign!!!!! ")
+        return
 
+    # fyi: checking using is dearpygui running before creating everythign will give it a heart attack
+    try:
         create_window()
         dpg.setup_dearpygui()
         dpg.show_viewport()
+        with dpg.font_registry():
+            heading_font = dpg.add_font("C:/Windows/Fonts/arial.ttf", size=24)
+        dpg.bind_item_font("h", heading_font)
         dpg.set_primary_window("closta", True)
         WINDOW_RUNNING = True
 
-        # gotta check for the inial focus to then check for unfocus,
-        # otherwise itll kill itself instantly
+        # first check to see if you have decided to click on closta yet
         while dpg.is_dearpygui_running():
             activ = pwc.getActiveWindowTitle()
-            if _i_am_closed:
+            if _graceful_tray_exit:
                 break
             if activ == "closta":
                 break
@@ -63,17 +116,16 @@ def spawn_window():
         # breaks if window isnt focused.
         while dpg.is_dearpygui_running():
             activ = pwc.getActiveWindowTitle()
-            if _i_am_closed:
+            if _graceful_tray_exit:
                 break
             if activ != "closta":
                 break
             dpg.render_dearpygui_frame()
-        dpg.destroy_context()
+
+    finally:
         WINDOW_RUNNING = False
-    print("window is runnign!!!!! ")
-
-
+        dpg.destroy_context()
+        
 if __name__ == "__main__":
-    print("sanity check")
     spawn_window()
 
